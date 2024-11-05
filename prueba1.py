ce77634e01934fb78368535e670322eae439a3e8
Vanesa Rioja <vanesa.rioja@upch.pe>
2024-11-05 07:14:06 +0000

    mapa

--- prueba1.py
+++ prueba1.py
@@ -6,3 +6,8 @@ st.title("Proyecto Programación")
 n = st.slider("n", 5, 100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n), columns=['data'])
 st.line_chart(chart_data)
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+st.map(df)
